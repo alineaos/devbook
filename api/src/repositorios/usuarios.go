7bf9f864b1c562cbb168c0d536e8515ba9be312a
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-21 19:18:26 -0300

    feat: Atualização de Usuário

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -101,3 +101,20 @@ func (repositorio Usuarios) BuscarPorID(ID uint64) (modelos.Usuario, error) {
 	}
 	return usuario, nil
 }
+
+// Atualizar altera as informações de um usuário no banco de dados
+func (repositorio Usuarios) Atualizar(ID uint64, usuario modelos.Usuario) error {
+	statement, erro := repositorio.db.Prepare(
+		"UPDATE usuarios SET nome = ?, username = ?, email = ? WHERE id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuario.Nome, usuario.Username, usuario.Email, ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
