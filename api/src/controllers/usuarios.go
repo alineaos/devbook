c0c13d622f62019e0dbeb7d759ff910dc2b5bfb2
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-21 17:47:20 -0300

    feat: Pacote Repositórios

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,10 +1,36 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando Usuário!"))
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	repositorio.Criar(usuario)
+
 }
 
 // BuscarUsuarios busca todos os usuários salvos banco de dados
