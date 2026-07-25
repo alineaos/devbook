e453e2ea47b63897c67a081d611871e334e1fac7
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-25 11:25:44 -0300

    feat: Parar de Seguir Usuário

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -167,3 +167,18 @@ func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+// Seguir permite que um usuário pare de seguir outro
+func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare("DELETE FROM seguidores WHERE usuario_id = ? AND seguidor_id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
