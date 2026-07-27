dfd93539a27358d148ade09438ad6d41353b0b84
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-27 18:06:12 -0300

    feat: Rotas de Publicações

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao adiciona uma nova publicação no Banco de Dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacoes retorna todas as publicações que apareceriam no feed do usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacao retorna uma única publicação
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// AtualizarPublicacao altera os dados de uma publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletarPublicacao exclui os dados de uma publicação
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
