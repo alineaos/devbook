956dd1e15da7be3c06a4c19f5802ecff60315fc2
Aline <alineoliveiradesantana2013@gmail.com>
2026-07-27 17:59:11 -0300

    feat: Modelo de Publicação

--- api/sql/init.sql
+++ api/sql/init.sql
@@ -2,7 +2,10 @@ CREATE DATABASE IF NOT EXISTS devbook;
 
 USE devbook;
 
+DROP TABLE IF EXISTS publicacoes;
+
 DROP TABLE IF EXISTS seguidores;
+
 DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
@@ -20,4 +23,14 @@ CREATE TABLE seguidores(
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
     FOREIGN KEY (seguidor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
     PRIMARY KEY(usuario_id, seguidor_id)
+) ENGINE = INNODB;
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    autor_id int not null,
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp,
+    FOREIGN KEY (autor_id) REFERENCES usuarios(id) ON DELETE CASCADE
 ) ENGINE = INNODB;
